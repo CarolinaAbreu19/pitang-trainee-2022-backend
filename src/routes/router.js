1a94959dfeeb373f2f1cb0bb93f935d0076d0217
Carolina Abreu <anacarolinaks19@gmail.com>
2022-04-11 20:27:22 -0300

    feat: import registerAppointment controller

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,5 +1,8 @@
 import { Router } from "express";
+import registerAppointment from "../controllers/registerAppointment.js";
 
 const router = Router();
+
+router.post('/appointment', registerAppointment);
 
 export default router;
