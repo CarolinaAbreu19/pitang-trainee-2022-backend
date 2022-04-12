363a4fe6a3381a950cc700c2d7fe9024f6031e46
Carolina Abreu <anacarolinaks19@gmail.com>
2022-04-12 15:15:23 -0300

    fix: birth_date and date_appointment validations

--- src/controllers/registerAppointment.js
+++ src/controllers/registerAppointment.js
@@ -1,40 +1,42 @@
 import appointments from "../models/appointmentModel.js";
+import crypto from "crypto";
 
 const registerAppointment = async (req, res) => {
-    const { name, birth_date, day, time } = req.body;
+    const { name, birth_date, date_appointment, time_appointment } = req.body;
 
     if(!name) {
         return res.status(400).json({ message: "Name field is required" });
     }
-    if(!birth_date) {
+    if(!birth_date || birth_date === 'NaN/NaN/NaN') {
         return res.status(400).json({ message: "Birth date field is required" });
     }
-    if(!day) {
-        return res.status(400).json({ message: "Day field is required" });
+    if(!date_appointment || date_appointment === 'NaN/NaN/NaN') {
+        return res.status(400).json({ message: "Date appointment field is required" });
     }
-    if(!time) {
-        return res.status(400).json({ message: "Time field is required" });
+    if(!time_appointment) {
+        return res.status(400).json({ message: "Time appointment field is required" });
     }
     
-    const filterDays = await appointments.filter(appointment => appointment.day === day);
+    const filterDays = await appointments.filter(appointment => appointment.date_appointment === date_appointment);
     if(filterDays.length >= 20) {
         return res.status(400).json({ message: "No more schedules for this day" });
     }
 
-    const filterSchedule = await appointments.filter(appointment => appointment.day === day && appointment.time === time);
+    const filterSchedule = filterDays.filter(appointment => appointment.time_appointment === time_appointment);
     if(filterSchedule.length >= 2) {
-        return res.status(400).json({ message: "No vacancy for this schedule" });
+        return res.status(400).json({ message: "No more available appointments for this schedule" });
     }
 
-
-    const newUser = await appointments.push({
+    await appointments.push({
+        id: crypto.randomUUID(),
         name: name,
         birth_date: birth_date,
-        day: day,
-        time: time,
+        date_appointment: date_appointment,
+        time_appointment: time_appointment,
+        situation: "waiting"
     });
 
-    return res.status(200).json({ message: "ok", appointment: appointments });
+    return res.status(201).json({ message: "Appointment registered successfully", appointment: appointments });
 }
 
 export default registerAppointment;
