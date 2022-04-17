f5e052bfdbe4e9eac246949e4f864b1f91598739
Carolina Abreu <anacarolinaks19@gmail.com>
2022-04-17 14:54:28 -0300

    feat: delete appointment endpoint created

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
+import deleteAppointment from "../controllers/deleteAppointment.js";
 import listAppointments from "../controllers/listAppointments.js";
 import registerAppointment from "../controllers/registerAppointment.js";
 
 const router = Router();
 
-router.post('/appointment', registerAppointment);
 router.get('/appointment', listAppointments);
+router.post('/appointment', registerAppointment);
+router.delete('/appointment/:id', deleteAppointment);
 
 export default router;
