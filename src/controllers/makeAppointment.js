720eef58469f41b5ca6f8df36cc24c706a1d9221
Carolina Abreu <anacarolinaks19@gmail.com>
2022-04-22 17:49:20 -0300

    feat: function makeAppointment created

---
+++ src/controllers/makeAppointment.js
@@ -0,0 +1,20 @@
+import appointments from "../models/appointmentModel.js";
+
+const makeAppointment = async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        const appointmentFound = await appointments.find(appointment => appointment.id === id);
+        if(!appointmentFound) {
+            return res.status(404).json({ message: "Appointment not found" });
+        }
+
+        appointmentFound.situation = 'done';
+
+        return res.status(200).json({ message: "Appointment done", appointment: appointmentFound });
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+export default makeAppointment;
