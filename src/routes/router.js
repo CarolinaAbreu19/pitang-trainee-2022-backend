0409e905a283d0703a63c1471e7b2744c24f3c05
Carolina Abreu <anacarolinaks19@gmail.com>
2022-04-22 17:48:37 -0300

    feat: endpoint makeAppointment created

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,12 +1,14 @@
 import { Router } from "express";
 import deleteAppointment from "../controllers/deleteAppointment.js";
 import listAppointments from "../controllers/listAppointments.js";
+import makeAppointment from "../controllers/makeAppointment.js";
 import registerAppointment from "../controllers/registerAppointment.js";
 
 const router = Router();
 
 router.get('/appointment', listAppointments);
 router.post('/appointment', registerAppointment);
+router.patch('/appointment/:id', makeAppointment);
 router.delete('/appointment/:id', deleteAppointment);
 
 export default router;
