1c86ba7ff642cc6ea67d61de797a9d878db02ff7
Carolina Abreu <anacarolinaks19@gmail.com>
2022-04-23 16:55:09 -0300

    test: registerAppointments with some required fields missing

--- src/tests/appointments.test.js
+++ src/tests/appointments.test.js
@@ -1,8 +1,20 @@
 import request from 'supertest';
 import app from '../app';
 
+describe('GET /appointment', () => {
+    test("List all appointments. There must be no appointments when starting the server", async () => {
+        const listAppointments = await request(app).get('/appointment');
+
+        expect(listAppointments.body).toHaveProperty("message");
+        expect(listAppointments.body).toHaveProperty("appointments");
+        expect(listAppointments.body.message).toBe("No results");
+        expect(listAppointments.body.appointments.length).toBe(0);
+        expect(listAppointments.statusCode).toBe(200);
+    });
+});
+
 describe('POST /appointment', () => {
-    test('Normal requisition, all inputs correctly informed', async () => {
+    test('Normal requisition, all inputs correctly informed. Must return status code 201', async () => {
         const newAppointment = await request(app)
             .post('/appointment')
             .send({
@@ -54,6 +66,84 @@ describe('POST /appointment', () => {
         expect(newAppointment.body).toHaveProperty("error_path");
         expect(newAppointment.body.message).toBe("name is a required field");
         expect(newAppointment.body.error_path).toBe("name");
+        expect(newAppointment.statusCode).toBe(400);
+
+        const response = await request(app).get("/appointment");
+
+            expect(response.body).toHaveProperty("message");
+            expect(response.body).toHaveProperty("appointments");
+            expect(response.body.appointments.length).toBe(1);
+
+            expect(response.statusCode).toBe(200);
+    });
+});
+
+describe('POST /appointment', () => {
+    test('Trying to make an appointment without the birth_date field. Must return error 400', async () => {
+        const newAppointment = await request(app)
+            .post('/appointment')
+            .send({
+                name: 'jest test',
+                date_appointment: '23/04/2023',
+                time_appointment: 8
+            });
+
+        expect(newAppointment.body).toHaveProperty("message");
+        expect(newAppointment.body).toHaveProperty("error_path");
+        expect(newAppointment.body.message).toBe("birth_date is a required field");
+        expect(newAppointment.body.error_path).toBe("birth_date");
+        expect(newAppointment.statusCode).toBe(400);
+
+        const response = await request(app).get("/appointment");
+
+            expect(response.body).toHaveProperty("message");
+            expect(response.body).toHaveProperty("appointments");
+            expect(response.body.appointments.length).toBe(1);
+
+            expect(response.statusCode).toBe(200);
+    });
+});
+
+describe('POST /appointment', () => {
+    test('Trying to make an appointment without the date_appointment field. Must return error 400', async () => {
+        const newAppointment = await request(app)
+            .post('/appointment')
+            .send({
+                name: 'jest test',
+                birth_date: '23/04/2023',
+                time_appointment: 8
+            });
+
+        expect(newAppointment.body).toHaveProperty("message");
+        expect(newAppointment.body).toHaveProperty("error_path");
+        expect(newAppointment.body.message).toBe("date_appointment is a required field");
+        expect(newAppointment.body.error_path).toBe("date_appointment");
+        expect(newAppointment.statusCode).toBe(400);
+
+        const response = await request(app).get("/appointment");
+
+            expect(response.body).toHaveProperty("message");
+            expect(response.body).toHaveProperty("appointments");
+            expect(response.body.appointments.length).toBe(1);
+
+            expect(response.statusCode).toBe(200);
+    });
+});
+
+describe('POST /appointment', () => {
+    test('Trying to make an appointment without the time_appointment field. Must return error 400', async () => {
+        const newAppointment = await request(app)
+            .post('/appointment')
+            .send({
+                name: 'jest test',
+                birth_date: '23/04/2022',
+                date_appointment: '23/04/2023'
+            });
+
+        expect(newAppointment.body).toHaveProperty("message");
+        expect(newAppointment.body).toHaveProperty("error_path");
+        expect(newAppointment.body.message).toBe("time_appointment is a required field");
+        expect(newAppointment.body.error_path).toBe("time_appointment");
         expect(newAppointment.statusCode).toBe(400);
 
         const response = await request(app).get("/appointment");
