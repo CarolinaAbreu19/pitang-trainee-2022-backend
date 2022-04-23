40bc60daf94aa3969e26ebff6ebab104b71f6625
Carolina Abreu <anacarolinaks19@gmail.com>
2022-04-23 07:30:24 -0300

    feat: use validateBodyMiddleware on registerAppointment requisition

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,13 +1,15 @@
 import { Router } from "express";
+import appointmentSchema from "../config/validations.js";
 import deleteAppointment from "../controllers/deleteAppointment.js";
 import listAppointments from "../controllers/listAppointments.js";
 import makeAppointment from "../controllers/makeAppointment.js";
 import registerAppointment from "../controllers/registerAppointment.js";
+import validateBodyMiddleware from "../middlewares/validateBodyMiddleware.js";
 
 const router = Router();
 
 router.get('/appointment', listAppointments);
-router.post('/appointment', registerAppointment);
+router.post('/appointment', validateBodyMiddleware(appointmentSchema), registerAppointment);
 router.patch('/appointment/:id', makeAppointment);
 router.delete('/appointment/:id', deleteAppointment);
 
