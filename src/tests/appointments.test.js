30322573cb608c1c4b4b191c0edc3c4042281ea9
Carolina Abreu <anacarolinaks19@gmail.com>
2022-04-23 18:40:50 -0300

    test: listAppointment | filter by date - no results

--- src/tests/appointments.test.js
+++ src/tests/appointments.test.js
@@ -269,3 +269,14 @@ describe('GET /appointment', () => {
         expect(filterAnotherDate.statusCode).toBe(200);
     });
 });
+
+describe('GET /appointment', () => {
+    test('Filter appointments by date, but there is no results', async () => {
+        const filterDate = await request(app).get('/appointment?filter=date&value=20/04/2023');        
+        expect(filterDate.body).toHaveProperty("message");
+        expect(filterDate.body).toHaveProperty("appointments");
+        expect(filterDate.body.message).toBe("No results");
+        expect(filterDate.body.appointments.length).toBe(0);
+        expect(filterDate.statusCode).toBe(200);
+    });
+});
